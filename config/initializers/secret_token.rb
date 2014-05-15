c69b1d9c676a8d1ca760d428ff9be37176584465
venkatprasath <venkatprasath.perumal@listertechnologies.com>
2014-05-15 14:28:09 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Illuminate::Application.config.secret_token = 'b513d8ff4c71972dff2182ca22148d066bb235cbcf532c016e9856e8587396ffa8b9ee665f83f68c3a889678971f070890dafcc3c8b0414b820efe75bc6cd028'
